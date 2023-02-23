519a784e9dfd5596a6ac1f75d55cc95f92b67884
Code Family <115712308+Codefamily1@users.noreply.github.com>
2023-02-24 00:04:42 +0530

    Revert "increased font size and changed the button color to gradient"

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
+import logo from './logo.svg';
 import './App.css';
-import Navbar from './components/Navbar';
-import Home from './pages/Home';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 function App() {
   return (
     <div className="App">
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route path='/' element={<Home />}/>
-      </Routes>
-    </Router>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
