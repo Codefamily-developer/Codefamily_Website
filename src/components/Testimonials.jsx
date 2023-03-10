865aa43ac187544f7f08dbfa45993d1f24065b0c
Nidhish Chauhan <rch246chauhan@gmail.com>
2023-03-10 19:11:03 +0530

    testimonial section changed

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -9,176 +9,61 @@ import "swiper/css/pagination";
 
 import "../styles/Testimonials.css";
 
-// import required modules
-import { EffectCoverflow, Pagination } from "swiper";
-
 import RightDoubleQuote from "../assets/icons/quotation.png";
 import User1 from "../assets/images/profile.png";
 
 export default function Testimonials() {
   return (
     <React.Fragment>
-      <h1>Testimonials</h1>
-      <Swiper
-        effect={"coverflow"}
-        grabCursor={true}
-        centeredSlides={true}
-        slidesPerView={"auto"}
-        pagination={true}
-        coverflowEffect={{
-          rotate: 0,
-          stretch: 0,
-          depth: 100,
-          modifier: 1,
-          slideShadows: true,
-        }}
-        modules={[EffectCoverflow, Pagination]}
-        className="mySwiper"
-      >
-        <SwiperSlide>
-          <div className="testimonial-box">
-            {/* <img src={RightDoubleQuote} alt="" /> */}
-            <div className="review">
-              <p>
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magni
-                sed debitis vitae atque nobis sit recusandae fugit fuga ea
-                molestiae! Numquam itaque quas perferendis veniam nostrum
-                corrupti? Vel dolorum recusandae adipisci officia nesciunt
-                voluptate alias.
-              </p>
-              <div className="reviewer">
-                <div className="profilePic">
-                  <img src={User1} alt="" />
-                </div>
+      <div className="testimonials">
+        <div className="container">
+          <h1>Testimonials</h1>
+          <div className="cards">
+            <div className="card">
+              <div className="profile">
+                <img src={User1} alt="" className="user" />
+                <blockquote>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Incidunt possimus commodi obcaecati, natus at exercitationem
+                  cumque sint aut quia ullam optio voluptatum, culpa temporibus
+                  dicta?
+                </blockquote>
                 <h3>
-                  John Doe <br />
-                  <span>Developer</span>
+                  John Doe <span>Developer, XYZ</span>
+                </h3>
+              </div>
+            </div>
+            <div className="card">
+              <div className="profile">
+                <img src={User1} alt="" className="user" />
+                <blockquote>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Incidunt possimus commodi obcaecati, natus at exercitationem
+                  cumque sint aut quia ullam optio voluptatum, culpa temporibus
+                  dicta?
+                </blockquote>
+                <h3>
+                  John Doe <span>Developer, XYZ</span>
+                </h3>
+              </div>
+            </div>
+            <div className="card">
+              <div className="profile">
+                <img src={User1} alt="" className="user" />
+                <blockquote>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Incidunt possimus commodi obcaecati, natus at exercitationem
+                  cumque sint aut quia ullam optio voluptatum, culpa temporibus
+                  dicta?
+                </blockquote>
+                <h3>
+                  John Doe <span>Developer, XYZ</span>
                 </h3>
               </div>
             </div>
           </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          {" "}
-          <div className="testimonial-box">
-            {/* <img src={RightDoubleQuote} alt="" /> */}
-            <div className="review">
-              <p>
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magni
-                sed debitis vitae atque nobis sit recusandae fugit fuga ea
-                molestiae! Numquam itaque quas perferendis veniam nostrum
-                corrupti? Vel dolorum recusandae adipisci officia nesciunt
-                voluptate alias.
-              </p>
-              <div className="reviewer">
-                <div className="profilePic">
-                  <img src={User1} alt="" />
-                </div>
-                <h3>
-                  John Doe <br />
-                  <span>Developer</span>
-                </h3>
-              </div>
-            </div>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          {" "}
-          <div className="testimonial-box">
-            {/* <img src={RightDoubleQuote} alt="" /> */}
-            <div className="review">
-              <p>
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magni
-                sed debitis vitae atque nobis sit recusandae fugit fuga ea
-                molestiae! Numquam itaque quas perferendis veniam nostrum
-                corrupti? Vel dolorum recusandae adipisci officia nesciunt
-                voluptate alias.
-              </p>
-              <div className="reviewer">
-                <div className="profilePic">
-                  <img src={User1} alt="" />
-                </div>
-                <h3>
-                  John Doe <br />
-                  <span>Developer</span>
-                </h3>
-              </div>
-            </div>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          {" "}
-          <div className="testimonial-box">
-            {/* <img src={RightDoubleQuote} alt="" /> */}
-            <div className="review">
-              <p>
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magni
-                sed debitis vitae atque nobis sit recusandae fugit fuga ea
-                molestiae! Numquam itaque quas perferendis veniam nostrum
-                corrupti? Vel dolorum recusandae adipisci officia nesciunt
-                voluptate alias.
-              </p>
-              <div className="reviewer">
-                <div className="profilePic">
-                  <img src={User1} alt="" />
-                </div>
-                <h3>
-                  John Doe <br />
-                  <span>Developer</span>
-                </h3>
-              </div>
-            </div>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          {" "}
-          <div className="testimonial-box">
-            {/* <img src={RightDoubleQuote} alt="" /> */}
-            <div className="review">
-              <p>
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magni
-                sed debitis vitae atque nobis sit recusandae fugit fuga ea
-                molestiae! Numquam itaque quas perferendis veniam nostrum
-                corrupti? Vel dolorum recusandae adipisci officia nesciunt
-                voluptate alias.
-              </p>
-              <div className="reviewer">
-                <div className="profilePic">
-                  <img src={User1} alt="" />
-                </div>
-                <h3>
-                  John Doe <br />
-                  <span>Developer</span>
-                </h3>
-              </div>
-            </div>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          {" "}
-          <div className="testimonial-box">
-            {/* <img src={RightDoubleQuote} alt="" /> */}
-            <div className="review">
-              <p>
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magni
-                sed debitis vitae atque nobis sit recusandae fugit fuga ea
-                molestiae! Numquam itaque quas perferendis veniam nostrum
-                corrupti? Vel dolorum recusandae adipisci officia nesciunt
-                voluptate alias.
-              </p>
-              <div className="reviewer">
-                <div className="profilePic">
-                  <img src={User1} alt="" />
-                </div>
-                <h3>
-                  John Doe <br />
-                  <span>Developer</span>
-                </h3>
-              </div>
-            </div>
-          </div>
-        </SwiperSlide>
-      </Swiper>
+        </div>
+      </div>
     </React.Fragment>
   );
 }
