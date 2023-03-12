6bfb9c419fc816a2d35aca7b2339baf81b3014c5
Nidhish Chauhan <rch246chauhan@gmail.com>
2023-03-12 13:57:58 +0530

    evnt card done

--- src/components/Events.jsx
+++ src/components/Events.jsx
@@ -2,29 +2,30 @@ import React from "react";
 import { ImBullhorn } from "react-icons/im";
 
 import EventImg from "../assets/images/event.jpg";
+import styles from "../styles/Event.module.css";
 
 const Events = () => {
   return (
-    <div className="events">
-      <div className="eventImg">
+    <div className={styles.events}>
+      <div className={styles.eventImg}>
         <img src={EventImg} alt="" width={"450px"} />
       </div>
-      <div className="event_data">
+      <div className={styles.event_data}>
         <div>
           <h1>Events</h1>
           <p>Empowering Tech Minds</p>
         </div>
 
-        <div>
-          <div>
+        <div className={styles.event_quotes}>
+          <div className={styles.eventQuote}>
             <ImBullhorn color="#f4c01e" size={24} />
             <h2>Exciting activities and challenges</h2>
           </div>
-          <div>
+          <div className={styles.eventQuote}>
             <ImBullhorn color="#f4c01e" size={24} />
             <h2>Exciting activities and challenges</h2>
           </div>
-          <div>
+          <div className={styles.eventQuote}>
             <ImBullhorn color="#f4c01e" size={24} />
             <h2>Opportunities to network and collaborate</h2>
           </div>
