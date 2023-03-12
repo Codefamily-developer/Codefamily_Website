cb4a9ea0b7450fbbf8870d809d3a0df2243f1817
Nidhish Chauhan <rch246chauhan@gmail.com>
2023-03-12 22:49:56 +0530

    responsive navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,21 +1,45 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import '../styles/Navbar.css'
+import React from "react";
+import { Link } from "react-router-dom";
+import styles from "../styles/Navbar.module.css";
+import { FaBars, FaTimes } from "react-icons/fa";
 const Navbar = () => {
+  const [active, setActive] = React.useState(false);
   return (
-    <div className="navbar">
-        <div className="navLeft">
-            <Link to="/">Code Family</Link>
-        </div>
-        <div className="navRight">
-            <Link to="/resources">Resources</Link>
-            <Link to="/events">Events</Link>
-            <Link to="/blogs">Blogs</Link>
-            <Link to="/community">Community</Link>
-            <button>Partner with us</button>
-        </div>
-    </div>
-  )
-}
+    <div className={styles.navbar}>
+      <div className={styles.navLeft}>
+        <Link to="/">Code Family</Link>
+      </div>
 
-export default Navbar
+      <ul
+        className={`${active ? [styles.responsive_nav] : styles.navRight}`}
+        onClick={() => {
+          setActive(false);
+        }}
+      >
+        <Link to="/resources">
+          <li>Resources</li>
+        </Link>
+        <Link to="/events">
+          <li>Events</li>
+        </Link>
+        <Link to="/blogs">
+          <li>Blogs</li>
+        </Link>
+        <Link to="/community">
+          <li>Community</li>
+        </Link>
+        <button className={styles.partner}>Partner with us</button>
+      </ul>
+      <button
+        className={styles.mobile_menu}
+        onClick={() => {
+          setActive(!active);
+        }}
+      >
+        {active ? <FaTimes></FaTimes> : <FaBars />}
+      </button>
+    </div>
+  );
+};
+
+export default Navbar;
